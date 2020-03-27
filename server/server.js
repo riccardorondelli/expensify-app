1f093ddddf73e18895731ad9a93c1969019b9a85
riccardorondelli <riccardorondelli0@gmail.com>
2020-03-27 16:03:35 +0100

    Heroku deploy

--- server/server.js
+++ server/server.js
@@ -7,17 +7,9 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-<<<<<<< HEAD
   res.sendFile(path.join(publicPath, 'index.html'));
-=======
-  res.sendFile(path.join(publicPath, 'index.html'))
->>>>>>> b654511824d8d542aed674707059cbd03c3dcb09
 });
 
 app.listen(port, () => {
   console.log('Server is up!');
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> b654511824d8d542aed674707059cbd03c3dcb09
